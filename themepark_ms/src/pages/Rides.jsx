3f6f9144cf392fb1da89ddd2aa47a2e8ef85dc66
Cole Plagens <142531035+Colep39@users.noreply.github.com>
2025-03-25 15:38:13 -0500

    fixing images extended

--- themepark_ms/src/pages/Rides.jsx
+++ themepark_ms/src/pages/Rides.jsx
@@ -5,23 +5,23 @@ export default function Rides() {
   return (
     <div className="ride-component-container">
       <div className="img-container">
-        <img src="public/images/main-rides-img.jpeg" alt="Themepark Image" className="main-ride-img"></img>
+        <img src="/images/main-rides-img.jpeg" alt="Themepark Image" className="main-ride-img"></img>
         <div className="text-overlay">Rides</div>
       </div>
 
       <div className="rides-container">
-        <Ride name="UmaNator" src="public/images/ferris-wheel.jpg"/>
-        <Ride name="UmaCoaster 3000" src="public/images//roller-coaster.jpeg"/>
-        <Ride name="UmaGeddon" src="public/images/freefall.webp"/>
-        <Ride name="Umapocalypse" src="public/images/umas-bullet.jpeg"/>
-        <Ride name="Uma's Infinite Loop" src="public/images/umas-infinite-loop.webp"/>
-        <Ride name="UmaPhobia" src="public/images/twirly-thing.jpg"/>
-        <Ride name="OctoUma" src="public/images/kid-ride1.jpg"/>
-        <Ride name="Wheel O Uma Jr" src="public/images/kid-ride2.jpeg"/>
-        <Ride name="Heavens Fall" src="public/images/fall-of-uma.jpg" />
-        <Ride name="Umanji" src="public/images/eye-of-uma.jpg"/>
-        <Ride name="BumperUma" src="public/images/bumper-boats.webp"/>
-        <Ride name="UmaLoopa" src="public/images/coaster-train.jpg"/>
+        <Ride name="UmaNator" src="/public/images/ferris-wheel.jpg"/>
+        <Ride name="UmaCoaster 3000" src="/public/images//roller-coaster.jpeg"/>
+        <Ride name="UmaGeddon" src="/public/images/freefall.webp"/>
+        <Ride name="Umapocalypse" src="/public/images/umas-bullet.jpeg"/>
+        <Ride name="Uma's Infinite Loop" src="/public/images/umas-infinite-loop.webp"/>
+        <Ride name="UmaPhobia" src="/public/images/twirly-thing.jpg"/>
+        <Ride name="OctoUma" src="/public/images/kid-ride1.jpg"/>
+        <Ride name="Wheel O Uma Jr" src="/public/images/kid-ride2.jpeg"/>
+        <Ride name="Heavens Fall" src="/public/images/fall-of-uma.jpg" />
+        <Ride name="Umanji" src="/public/images/eye-of-uma.jpg"/>
+        <Ride name="BumperUma" src="/public/images/bumper-boats.webp"/>
+        <Ride name="UmaLoopa" src="/public/images/coaster-train.jpg"/>
       </div>
     </div>
   )
--- themepark_ms/src/pages/Shops/AdventureGear.jsx
+++ themepark_ms/src/pages/Shops/AdventureGear.jsx
@@ -8,18 +8,18 @@ export default function AdventureGear(){
                 <h1 id="shop-header">Adventure Gear - Featured Items</h1>
 
                 <div className="shop-items-container">
-                    <ShopItem src="public/images/nah-id-win-shirt.jpeg" shopItemName="Gojos Declaration" price="15.00"/>
-                    <ShopItem src="public/images/beach-ball.avif" shopItemName="Beach Ball" price="5.00"/>
-                    <ShopItem src="public/images/uma-zooma-hat.webp" shopItemName="UmaZooma Hat" price="10.00"/>
-                    <ShopItem src="public/images/flip-flops.jpg" shopItemName="Flip Flops" price="10.00"/>
-                    <ShopItem src="public/images/plush-small-lion.jpg" shopItemName="Speaking Lion" price="25.00" />
-                    <ShopItem src="public/images/sunglasses.jpg" shopItemName="Sunglasses" price="12.00"/>
-                    <ShopItem src="public/images/sunscreen.jpg" shopItemName="Sun Screen" price="8.00"/>
-                    <ShopItem src="public/images/water noodle.jpg" shopItemName="Water Noodle" price="6.00"/>
-                    <ShopItem src="public/images/swim-trunks.webp" shopItemName="Swim Trunks" price="18.00"/>
-                    <ShopItem src="public/images/water-gun1.jpg" shopItemName="Water Rifle" price="30.00"/>
-                    <ShopItem src="public/images/umabrella.webp" shopItemName="UmaBrella" price="20.00"/>
-                    <ShopItem src="public/images/water-gun2.jpg" shopItemName="Dual Blasters" price="19.00"/>
+                    <ShopItem src="/images/nah-id-win-shirt.jpeg" shopItemName="Gojos Declaration" price="15.00"/>
+                    <ShopItem src="/images/beach-ball.avif" shopItemName="Beach Ball" price="5.00"/>
+                    <ShopItem src="/images/uma-zooma-hat.webp" shopItemName="UmaZooma Hat" price="10.00"/>
+                    <ShopItem src="/images/flip-flops.jpg" shopItemName="Flip Flops" price="10.00"/>
+                    <ShopItem src="/images/plush-small-lion.jpg" shopItemName="Speaking Lion" price="25.00" />
+                    <ShopItem src="/images/sunglasses.jpg" shopItemName="Sunglasses" price="12.00"/>
+                    <ShopItem src="/images/sunscreen.jpg" shopItemName="Sun Screen" price="8.00"/>
+                    <ShopItem src="/images/water noodle.jpg" shopItemName="Water Noodle" price="6.00"/>
+                    <ShopItem src="/images/swim-trunks.webp" shopItemName="Swim Trunks" price="18.00"/>
+                    <ShopItem src="/images/water-gun1.jpg" shopItemName="Water Rifle" price="30.00"/>
+                    <ShopItem src="/images/umabrella.webp" shopItemName="UmaBrella" price="20.00"/>
+                    <ShopItem src="/images/water-gun2.jpg" shopItemName="Dual Blasters" price="19.00"/>
                 </div>
             </div>
         </>
