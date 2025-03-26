d895bcdf19c0f2b591c8829a80ffb329e2ece2d2
Eriklpz04 <eriklopez6876@gmail.com>
2025-03-25 22:50:39 -0500

    Add files via upload

---
+++ BackendGroup/BackendGroup/Models/Tickets.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace BackendGroup.Models
+{
+    [Table("ticket")]
+    public class Ticket
+    {
+        public enum TicketType
+        {
+            adult,
+            season,
+            youth,
+            child,
+            senior,
+            student
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ticket_id { get; set; }
+
+        // Foreign Key for User
+        public int? user_id { get; set; }
+
+        [ForeignKey("user_id")]
+        [JsonIgnore]  // Prevent circular reference
+        public virtual User? User { get; set; }
+
+        // Foreign Key for Ride
+        public int? ride_id { get; set; }
+
+        [ForeignKey("ride_id")]
+        [JsonIgnore]  // Prevent circular reference
+        public virtual Ride? Ride { get; set; }
+
+        public DateTime? Purchase_date { get; set; }
+        public TicketType? ticket_type { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
+
